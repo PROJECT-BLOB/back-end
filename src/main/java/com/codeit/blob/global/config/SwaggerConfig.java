81d27e12bdafefb4d7b50947e2f3943757fa2947
cherin kim <76779953+mawakeb@users.noreply.github.com>
2024-04-18 17:44:19 +0900

    Add basic config files

---
+++ src/main/java/com/codeit/blob/global/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.codeit.blob.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Blob API", description = "실시간 여행 정보 공유 사이트, Blob API 입니다.", version = "1.0"))
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+}
