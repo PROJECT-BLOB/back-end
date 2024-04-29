05e56f456f2f06343f19fd6f63f717d6dfccfebc
didado <idejrdud@gmail.com>
2024-04-29 18:45:34 +0900

    feat: #36 내가 쓴 게시물 조회 추가

--- src/main/java/com/codeit/blob/post/repository/PostJpaRepository.java
+++ src/main/java/com/codeit/blob/post/repository/PostJpaRepository.java
@@ -1,9 +1,14 @@
 package com.codeit.blob.post.repository;
 
 import com.codeit.blob.post.domain.Post;
+import com.codeit.blob.user.domain.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostJpaRepository extends JpaRepository<Post, Long> {
+    Page<Post> findPostByAuthor(Users users, Pageable pageable);
 }
